282ac9b97f1507fafade7db6efbc64c4466ea21d
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-02-02 17:51:51 -0300

    Add game mechanics to board module

--- script.js
+++ script.js
@@ -23,8 +23,48 @@ const gameBoard = (function() {
         }));
     };
 
+    let gameMechanics = function () {
+        let square = boardStructure;
+        for (let i = 0; i <= 3; i++) {
+            if (square[i][0] !== '' && square[i][0] === square[i][1] && square[i][1] === square[i][2]) {
+                if (square[i][0] === 'X') {
+                    console.log('You win!')
+                } 
+                else if (square[i][0] === 'O') {
+                    console.log('Computer wins!')
+                }
+            } 
+            else if (square[0][i] !== '' && square[0][i] === square[1][i] && square[1][i] === square[2][i]) {
+                if (square[0][i] === 'X') {
+                    console.log('You win!')
+                } 
+                else if (square[0][i] === 'O') {
+                    console.log('Computer wins!')
+                }
+            } 
+            else if (square[0][0] !== '' && square[0][0] === square[1][1] && square[1][1] === square[2][2]) {
+                if (square[0][0] === 'X') {
+                    console.log('You win!')
+                } 
+                else if (square[0][0] === 'O') {
+                    console.log('Computer wins!')
+                }
+            }
+            else if (square[0][2] !== '' && square[0][2] === square[1][1] && square[1][1] === square[2][0]) {
+                if (square[0][2] === 'X') {
+                    console.log('You win!')
+                } 
+                else if (square[0][2] === 'O') {
+                    console.log('Computer wins!')
+                }
+            }
+        }
+        
+    };
+
     return {
         boardMarking,
         boardStructure,
+        gameMechanics
     };
 })();
